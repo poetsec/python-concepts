ee1e8d5ae99498c008412880f4a7daee59ab30b0
Taylor Shakespear <52114607+poetsec@users.noreply.github.com>
2021-07-10 23:48:58 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 for i in range(1, 101):
-    if i % 3 -- 0 and i % 5 ==0:
+    if i % 3 == 0 and i % 5 == 0:
         print(str(i) + "fizzbuzz")
-    elif i % 3 ==0:
+    elif i % 3 == 0:
         print(str(i) + "fizz")
     elif i % 5 == 0:
         print(str(i) + "buzz")
