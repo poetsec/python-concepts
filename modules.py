251c337ec116e4ed2d0258ffaa62d730f47202cc
Taylor Shakespear <52114607+poetsec@users.noreply.github.com>
2021-07-10 21:55:59 -0600

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,14 @@
+# import module to use its functions
+import random
+random.randint(1, 10)
+
+# once the exit() function is called the program will terminate
+import sys
+print("Hello")
+sys.exit()
+print("Goodbye")
+
+# using third-party module pyperclip to copy and paste
+import pyperclip
+pyperclip.copy("Hellow world")
+pyperclip.paste()
